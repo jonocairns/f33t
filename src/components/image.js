9f64a5297fb9f486f5d3120ec2cdb948f73a9f30
Jono Cairns <jonocairns@gmail.com>
2019-08-23 23:15:38 +1200

    gogogo

--- src/components/image.js
+++ src/components/image.js
@@ -1,19 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import {useInterval} from './interval';
 
 const Image = () => {
+  const [img, setImg] = useState(undefined);
+
   const data = useStaticQuery(graphql`
     query {
       allFile {
@@ -35,7 +27,15 @@ const Image = () => {
       return Math.ceil(Math.random() * (count - 1))
     };
 
-  return <Img fluid={data.allFile.edges[fetchRandom(count)].node.childImageSharp.fluid} />
+    useInterval(() => {
+      setImg(data.allFile.edges[fetchRandom(count)].node.childImageSharp.fluid);
+    }, 1000);
+
+
+    if(!img) {
+      setImg(data.allFile.edges[fetchRandom(count)].node.childImageSharp.fluid)
+    }
+  return <Img fluid={img} />
 }
 
 export default Image
